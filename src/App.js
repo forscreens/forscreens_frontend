d4b3dbf7c0b1b0ca77d071b1013b0b997385b3c0
Rameshchandra Nerolu <rameshchandra85@gmail.com>
2023-08-02 00:10:58 +0530

    Login form

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ActorRegistration from './pages/ActorRegistration';
 import NavBar from './components/NavBar';
 import styles from './App.module.css';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
   console.log('Rendering App.js');
 
-  // When deploying to GitHub Pages, you should set the basename to your repository name.
-  // This is because GitHub Pages serves your app from a subdirectory that matches your repository name.
-  // So your app's root URL would be 'username.github.io/repository-name'.
-  // Therefore, the basename should be set to '/repository-name'.
-
-  // However, when you're using a custom domain, your app's root URL will be the domain root ('/').
-  // In this case, you should change the basename back to '/' or remove the basename prop from the Router entirely.
-
-  // Make sure to also update the 'homepage' field in your package.json file to match your app's root URL.
-
   return (
     <div className={styles.appContainer}>
       <Router basename="/forscreens_frontend">
-        <NavBar/>
+        <NavBar />
         <Routes>
           <Route path="/register" element={<ActorRegistration />} />
+          <Route path="/login" element={<LoginForm />} /> {/* Use 'element' prop here */}
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,61 @@
-// NavBar.jsx
-
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 import './NavBar.css';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleNavbarToggle = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleLinkClick = () => {
+    setExpanded(false);
+  };
+
   return (
-    <Navbar bg="white" expand="lg" className="mb-4">
+    <Navbar
+      bg="white"
+      expand="lg"
+      className="mb-4"
+      expanded={expanded}
+      onToggle={handleNavbarToggle}
+    >
       <Container>
-        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+        <Navbar.Brand href="#home" className="logo">
+          Logo
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="navbar-collapse">
-          <Nav className="mr-auto">
-            <Nav.Link href="#jobs">Find Jobs</Nav.Link>
-            <Nav.Link href="#talent">Find Talent</Nav.Link>
-            <Nav.Link href="#for-talent">For Talent</Nav.Link>
-            <Nav.Link href="#solutions">Casting/Hiring Solutions</Nav.Link>
-            <Nav.Link href="#magazine">Magazine</Nav.Link>
+          <Nav className="mr-auto nav-links">
+            <Nav.Link href="#jobs" onClick={handleLinkClick}>
+              Find Jobs
+            </Nav.Link>
+            <Nav.Link href="#talent" onClick={handleLinkClick}>
+              Find Talent
+            </Nav.Link>
+            <Nav.Link href="#for-talent" onClick={handleLinkClick}>
+              For Talent
+            </Nav.Link>
+            <Nav.Link href="#solutions" onClick={handleLinkClick}>
+              Casting/Hiring Solutions
+            </Nav.Link>
+            <Nav.Link href="#magazine" onClick={handleLinkClick}>
+              Magazine
+            </Nav.Link>
           </Nav>
-          <Nav>
-            <Button className="sign-in-btn">Sign In</Button>
+          <Nav className="sign-in-btn">
+            <Link to="/login">
+              <Button className="btn btn-primary" onClick={handleLinkClick}>
+                Sign In
+              </Button>
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
 
 export default NavBar;
