4cba624ee6138dd73dd5f58b5e0b4a19cb16f606
Rameshchandra Nerolu <rameshchandra85@gmail.com>
2023-08-09 20:51:10 +0530

    Actor page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NavBar from './components/NavBar';
 import styles from './App.module.css';
 import LoginForm from './components/LoginForm';
 import ActorRegistration from './components/ActorRegistration';
+import ProfilePage from './components/ActorPage';
 
 const App = () => {
   console.log('Rendering App.js');
@@ -18,6 +19,7 @@ const App = () => {
           <Route path="/register" element={<SignUp />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/register1" element={<ActorRegistration />} />
+          <Route path="/actorpage" element={<ProfilePage />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
